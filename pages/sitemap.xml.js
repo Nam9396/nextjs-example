5f5ed4c70f363f04b373adb7d4c89c0375ad7080
Nam9396 <nguyenthanhnam9396@gmail.com>
2023-03-07 13:16:49 +0700

    change sitemap function

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,46 +1,15 @@
-import { createSlugArr } from "../lib/function";
-
+import { createFinalSiteMap } from "../lib/function";
 
 const SiteMap = () => {};
 
 export default SiteMap;
 
-function generateSiteMap(slugArr) {
-    return `<?xml version="1.0" encoding="UTF-8"?>
-     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-       <!--We manually set the two URLs we know already-->
-       <url>
-         <loc>https://bacsichobeyeu.com/</loc>
-       </url>
-       <url>
-         <loc>https://bacsichobeyeu.com/phat-trien</loc>
-       </url>
-       <url>
-         <loc>https://bacsichobeyeu.com/cham-soc-tre</loc>
-       </url>
-       <url>
-         <loc>https://bacsichobeyeu.com/dinh-duong</loc>
-       </url>
-       ${slugArr
-         .map((slug) => {
-            return `
-             <url>
-               <loc>https://bacsichobeyeu.com/posts/${slug}</loc>
-             </url>
-            `;
-          })
-         .join('')}
-     </urlset>
-   `;
-}
-
 export const getServerSideProps = async ({ res }) => {
-  const slugArr = await createSlugArr();
-  const sitemap = generateSiteMap(slugArr);
+  const sitemap = await createFinalSiteMap();
   res.setHeader("Content-Type", "text/xml");
   res.write(sitemap);
   res.end();
   return {
     props: {},
-  };
+  }
 };
