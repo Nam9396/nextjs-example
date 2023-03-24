01343a4e413b7dde1f2a73a9ab0972393b4c41ea
Nam9396 <nguyenthanhnam9396@gmail.com>
2023-03-25 00:06:25 +0700

    test img link

--- components/nav/logo.js
+++ components/nav/logo.js
@@ -2,12 +2,14 @@ import clsx from "clsx";
 import Image from "next/image";
 import Link from "next/link";
 import data from "../../lib/pageData";
+import longlogo2 from "../../public/longlogo2.png"
 
 const Logo = () => {
   return (
     <Link href='/' >
       <Image 
-        src={data.nav.img}
+        // src={data.nav.img}
+        src={longlogo2}
         alt=""
         width={250}
         className=""
