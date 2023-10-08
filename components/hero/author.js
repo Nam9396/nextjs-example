153395fcdafd3238d8e8d919f890afec67206210
Nam9396 <nguyenthanhnam9396@gmail.com>
2023-10-09 00:19:44 +0700

    change author section

--- components/hero/author.js
+++ components/hero/author.js
@@ -5,13 +5,13 @@ import data from "../../lib/pageData";
 const Author = () => {
   return (
     <div className="w-fit h-fit box-row justify-start items-center ">
-      <Image
+      {/* <Image
         src={data.author.img}
         alt=""
         width={80}
         height={80}
         className="rounded-full mr-4"
-      />
+      /> */}
       <div className="w-fit h-fit box-col justify-center items-start">
         <ParaText content={data.author.p[0]} />
         <ParaText content={data.author.p[1]} />
